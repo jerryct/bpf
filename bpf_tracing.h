5bc563dd83047dcd5917f63a43b5c24870bdfe07
Jerry Crunchtime <jerry.c.t@web.de>
2020-07-19 16:27:02 +0200

    Apply clang-format

--- bpf_tracing.h
+++ bpf_tracing.h
@@ -4,46 +4,46 @@
 
 /* Scan the ARCH passed in from ARCH env variable (see Makefile) */
 #if defined(__TARGET_ARCH_x86)
-	#define bpf_target_x86
-	#define bpf_target_defined
+#define bpf_target_x86
+#define bpf_target_defined
 #elif defined(__TARGET_ARCH_s390)
-	#define bpf_target_s390
-	#define bpf_target_defined
+#define bpf_target_s390
+#define bpf_target_defined
 #elif defined(__TARGET_ARCH_arm)
-	#define bpf_target_arm
-	#define bpf_target_defined
+#define bpf_target_arm
+#define bpf_target_defined
 #elif defined(__TARGET_ARCH_arm64)
-	#define bpf_target_arm64
-	#define bpf_target_defined
+#define bpf_target_arm64
+#define bpf_target_defined
 #elif defined(__TARGET_ARCH_mips)
-	#define bpf_target_mips
-	#define bpf_target_defined
+#define bpf_target_mips
+#define bpf_target_defined
 #elif defined(__TARGET_ARCH_powerpc)
-	#define bpf_target_powerpc
-	#define bpf_target_defined
+#define bpf_target_powerpc
+#define bpf_target_defined
 #elif defined(__TARGET_ARCH_sparc)
-	#define bpf_target_sparc
-	#define bpf_target_defined
+#define bpf_target_sparc
+#define bpf_target_defined
 #else
-	#undef bpf_target_defined
+#undef bpf_target_defined
 #endif
 
 /* Fall back to what the compiler says */
 #ifndef bpf_target_defined
 #if defined(__x86_64__)
-	#define bpf_target_x86
+#define bpf_target_x86
 #elif defined(__s390__)
-	#define bpf_target_s390
+#define bpf_target_s390
 #elif defined(__arm__)
-	#define bpf_target_arm
+#define bpf_target_arm
 #elif defined(__aarch64__)
-	#define bpf_target_arm64
+#define bpf_target_arm64
 #elif defined(__mips__)
-	#define bpf_target_mips
+#define bpf_target_mips
 #elif defined(__powerpc__)
-	#define bpf_target_powerpc
+#define bpf_target_powerpc
 #elif defined(__sparc__)
-	#define bpf_target_sparc
+#define bpf_target_sparc
 #endif
 #endif
 
@@ -179,17 +179,15 @@ struct pt_regs;
 #endif
 
 #if defined(bpf_target_powerpc)
-#define BPF_KPROBE_READ_RET_IP(ip, ctx)		({ (ip) = (ctx)->link; })
-#define BPF_KRETPROBE_READ_RET_IP		BPF_KPROBE_READ_RET_IP
+#define BPF_KPROBE_READ_RET_IP(ip, ctx) ({ (ip) = (ctx)->link; })
+#define BPF_KRETPROBE_READ_RET_IP BPF_KPROBE_READ_RET_IP
 #elif defined(bpf_target_sparc)
-#define BPF_KPROBE_READ_RET_IP(ip, ctx)		({ (ip) = PT_REGS_RET(ctx); })
-#define BPF_KRETPROBE_READ_RET_IP		BPF_KPROBE_READ_RET_IP
+#define BPF_KPROBE_READ_RET_IP(ip, ctx) ({ (ip) = PT_REGS_RET(ctx); })
+#define BPF_KRETPROBE_READ_RET_IP BPF_KPROBE_READ_RET_IP
 #else
-#define BPF_KPROBE_READ_RET_IP(ip, ctx)					    \
-	({ bpf_probe_read(&(ip), sizeof(ip), (void *)PT_REGS_RET(ctx)); })
-#define BPF_KRETPROBE_READ_RET_IP(ip, ctx)				    \
-	({ bpf_probe_read(&(ip), sizeof(ip),				    \
-			  (void *)(PT_REGS_FP(ctx) + sizeof(ip))); })
+#define BPF_KPROBE_READ_RET_IP(ip, ctx) ({ bpf_probe_read(&(ip), sizeof(ip), (void *)PT_REGS_RET(ctx)); })
+#define BPF_KRETPROBE_READ_RET_IP(ip, ctx)                                                                             \
+  ({ bpf_probe_read(&(ip), sizeof(ip), (void *)(PT_REGS_FP(ctx) + sizeof(ip))); })
 #endif
 
 #endif
